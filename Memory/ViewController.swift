7886dafbbd64514f9908a14fe1960bf154ca484e
Chris <withscottc921@gmail.com>
2020-07-08 08:28:02 -0600

    Set up views and got buttons to flash

--- Memory/ViewController.swift
+++ Memory/ViewController.swift
@@ -10,6 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var greenButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var yellowButton: UIButton!
+    
+    
+    @IBAction func redButtonPressed(_ sender: UIButton) {
+        print("Red")
+        sender.flash()
+        
+    }
+    
+    
+    @IBAction func greenButtonPressed(_ sender: UIButton) {
+        print("green")
+        sender.flash()
+    }
+    
+    
+    @IBAction func blueButtonPressed(_ sender: UIButton) {
+        print("blue")
+        sender.flash()
+    }
+    
+    
+    @IBAction func yellowButtonPressed(_ sender: UIButton) {
+        print("yellow")
+        sender.flash()
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
