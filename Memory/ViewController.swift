1bfb9ccdb8fb2ebc1db1f042cea053b185ffc74e
Chris <withscottc921@gmail.com>
2020-07-09 08:18:26 -0600

    Added new level feature and verifying user patterns

--- Memory/ViewController.swift
+++ Memory/ViewController.swift
@@ -15,18 +15,30 @@ class ViewController: UIViewController {
     @IBOutlet weak var blueButton: UIButton!
     @IBOutlet weak var yellowButton: UIButton!
     
+    @IBOutlet weak var LevelLabel: UILabel!
+    @IBOutlet weak var GameInstructions: UILabel!
+    
     var currentLevel: Int = 0
     var highScore: Int = 0
     var currentPattern: [Int] = []
+    var userPattern: [Int] = []
     var currentIndex: Int = 0
+    var userIndex: Int = 0
     
     
     var timer = Timer()
     
+    @IBAction func NewGame(_ sender: UIButton) {
+        newLevel()
+        
+    }
     
     @IBAction func redButtonPressed(_ sender: UIButton) {
+        sender.flash()
         print("Red")
-        newLevel()
+        userPattern.append(0)
+        verifyPattern()
+        
         
     }
     
@@ -34,25 +46,34 @@ class ViewController: UIViewController {
     @IBAction func greenButtonPressed(_ sender: UIButton) {
         print("green")
         sender.flash()
+        userPattern.append(1)
+        verifyPattern()
     }
     
     
     @IBAction func blueButtonPressed(_ sender: UIButton) {
         print("blue")
         sender.flash()
+        userPattern.append(2)
+        verifyPattern()
     }
     
     
     @IBAction func yellowButtonPressed(_ sender: UIButton) {
         print("yellow")
         sender.flash()
+        userPattern.append(3)
+        verifyPattern()
     }
     
     
     func newLevel() {
         timer.invalidate()
         currentLevel += 1
+        LevelLabel.text = "Level: \(String(currentLevel))"
         currentIndex = 0
+        userIndex = 0
+        userPattern = []
         currentPattern.removeAll()
         for _ in 1...currentLevel {
             currentPattern.append(Int.random(in: 0 ..< 4))
@@ -66,15 +87,32 @@ class ViewController: UIViewController {
         
         
     @objc func onTimerFires(){
+        
         let colors = [redButton, greenButton, blueButton, yellowButton]
         
         if currentIndex == currentPattern.count {
             timer.invalidate()
+            view.isUserInteractionEnabled = true
+            GameInstructions.text = "Repeat Pattern"
         }else{
+            view.isUserInteractionEnabled = false
+            GameInstructions.text = "Watch Pattern"
             colors[currentPattern[currentIndex]]?.flash()
             currentIndex += 1
         }
         
+        
+        
+    }
+    
+    func verifyPattern() {
+        if userPattern.count > currentPattern.count || userPattern[userIndex] != currentPattern[userIndex] {
+            GameInstructions.text = "You lose!"
+            userPattern = []
+            currentLevel = 0
+        }else {
+            userIndex += 1
+        }
         
         
     }
