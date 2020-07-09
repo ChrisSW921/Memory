df9bcfb001fd700d1cfd23b0fb0dc91f99b4279d
Chris <withscottc921@gmail.com>
2020-07-08 21:52:32 -0600

    Added feature to get buttons to flash in random order and increment one flash per level

--- Memory/ViewController.swift
+++ Memory/ViewController.swift
@@ -15,10 +15,18 @@ class ViewController: UIViewController {
     @IBOutlet weak var blueButton: UIButton!
     @IBOutlet weak var yellowButton: UIButton!
     
+    var currentLevel: Int = 0
+    var highScore: Int = 0
+    var currentPattern: [Int] = []
+    var currentIndex: Int = 0
+    
+    
+    var timer = Timer()
+    
     
     @IBAction func redButtonPressed(_ sender: UIButton) {
         print("Red")
-        sender.flash()
+        newLevel()
         
     }
     
@@ -41,7 +49,35 @@ class ViewController: UIViewController {
     }
     
     
+    func newLevel() {
+        timer.invalidate()
+        currentLevel += 1
+        currentIndex = 0
+        currentPattern.removeAll()
+        for _ in 1...currentLevel {
+            currentPattern.append(Int.random(in: 0 ..< 4))
+        }
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(onTimerFires), userInfo: nil, repeats: true)
+    }
     
+    
+        
+        
+        
+        
+    @objc func onTimerFires(){
+        let colors = [redButton, greenButton, blueButton, yellowButton]
+        
+        if currentIndex == currentPattern.count {
+            timer.invalidate()
+        }else{
+            colors[currentPattern[currentIndex]]?.flash()
+            currentIndex += 1
+        }
+        
+        
+        
+    }
     
     
     override func viewDidLoad() {
